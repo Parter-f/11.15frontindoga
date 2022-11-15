f59e7b960d6bafd0e79e77f1137d082debff1e0c
Árpás Péter <arpaspeter0@gmail.com>
2022-11-15 09:23:30 +0100

    első feladat consolera elkészült

--- src/index.ts
+++ src/index.ts
@@ -9,23 +9,34 @@ interface QuoteList{
     quotes : Quote[];
 }
 
+const allQuoteBtn = document.getElementById('osszesIdezetGomb')
+allQuoteBtn?.addEventListener('click' , function ListAllQuote(){
+    
+})
+
 async function beolvas(){
     let response = await fetch("../quotes.json");
     let vals : QuoteList = await response.json();
     console.log(vals.quotes)
-    
-    vals.quotes.sort(function (a, b) {
-        if (a.author < b.author) {
-          return -1;
-        }
-        if (a.author > b.author) {
-          return 1;
-        }
-        return 0;
-      });
-      console.log(vals.quotes)
 
+    const allQuoteBtn = document.getElementById('osszesIdezetGomb')
+    allQuoteBtn?.addEventListener('click' , function ListAllQuote(){
+        vals.quotes.sort(function (a, b) {
+            if (a.author < b.author) {
+              return -1;
+            }
+            if (a.author > b.author) {
+              return 1;
+            }
+            return 0;
+          });
+          console.log(vals.quotes)
+})
+    
 }
+
+
+
 document.addEventListener('DOMContentLoaded',()=> {
     beolvas()
 })
